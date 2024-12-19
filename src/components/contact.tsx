ff06e6cb4ac4fac7f6d3489659374285ce7e1910
cavinam <cavinaditya5@gmail.com>
2024-12-19 15:31:06 +0700

    feat:fourth commit

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -5,6 +5,10 @@ export default function Contact() {
     <div
       className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-black to-gray-700 text-white"
       id="contact"
-    ></div>
+    >
+      <h2 className="text-center text-3xl font-bold xs:text-4xl md:text-5xl lg:text-6xl mb-10">
+        Contact Me
+      </h2>
+    </div>
   );
 }
--- src/components/testimony.tsx
+++ src/components/testimony.tsx
@@ -7,20 +7,25 @@ export default function Testimony() {
       className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-black to-gray-700 text-white"
       id="testimony"
     >
+      <h2 className="text-center text-3xl font-bold xs:text-4xl md:text-5xl lg:text-6xl mb-10">
+        Testimonial
+      </h2>
       <section className="bg-gradient-to-br from-black to-gray-700">
         <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
           <h2 className="text-center text-4xl font-bold tracking-tight text-white sm:text-5xl">
-            Read trusted reviews from our customers
+            Read trusted reviews from my customers
           </h2>
 
           <div className="mt-8 grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8">
             <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
               <div className="flex items-center gap-4">
-                <Image
+                {/* <Image
                   alt=""
                   src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
                   className="size-14 rounded-full object-cover"
-                />
+                  width={20}
+                  height={20}
+                /> */}
 
                 <div>
                   <div className="flex justify-center gap-0.5 text-green-500">
@@ -82,11 +87,13 @@ export default function Testimony() {
 
             <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
               <div className="flex items-center gap-4">
-                <Image
+                {/* <Image
                   alt=""
                   src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
                   className="size-14 rounded-full object-cover"
-                />
+                  width={20}
+                  height={20}
+                /> */}
 
                 <div>
                   <div className="flex justify-center gap-0.5 text-green-500">
@@ -148,11 +155,13 @@ export default function Testimony() {
 
             <blockquote className="rounded-lg bg-gray-50 p-6 shadow-sm sm:p-8">
               <div className="flex items-center gap-4">
-                <Image
+                {/* <Image
                   alt=""
                   src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                  width={20}
+                  height={20}
                   className="size-14 rounded-full object-cover"
-                />
+                /> */}
 
                 <div>
                   <div className="flex justify-center gap-0.5 text-green-500">
